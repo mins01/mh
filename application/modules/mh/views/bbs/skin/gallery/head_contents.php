30b3e27703c1e80926c1e2958d1c9d7f22d6ac26
mins01 <mins01@lycos.co.kr>
2015-12-22 23:16:40 +0900

    갤러리를 기본 게시판 스킨을 사용하도록..

--- application/modules/mh/views/bbs/skin/gallery/head_contents.php
+++ application/modules/mh/views/bbs/skin/gallery/head_contents.php
@@ -1,15 +1,2 @@
 <?
-
-$base_url = base_url();
-?>
-
-<!-- 게시판 추가 head_contents -->
-<link href="<?=html_escape(base_url('css/bbs/skin/bbs_skin_default.css'))?>" rel="stylesheet">
-<!-- //게시판 추가 head_contents -->
-<? if($mode=='read' && $bm_row['bm_use_comment']=='1'): ?>
-<script src="<?=html_escape(base_url('js/bbs/comment.js'))?>"></script>
-<? endif; ?>
-
-<? if($mode=='write' || $mode=='edit' || $mode=='answer'): ?>
-<script src="<?=html_escape(base_url('js/bbs/script.js'))?>"></script>
-<? endif; ?>
+require(dirname(__FILE__).'/../board/'.basename(__FILE__));
