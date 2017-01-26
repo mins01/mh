ee9de55065a0ea700296b2cd03f503bf1f95e113
mins01 <mins01.lycos.co.kr@gmail.com>
2017-01-26 18:05:31 +0900

    동작시 console.log 관련.

--- js/mh_def.js
+++ js/mh_def.js
@@ -1,1 +1,9 @@
 $('.selectpicker').selectpicker('mobile');
+(function(){
+	var tm = (new Date()).getTime();
+	window.addEventListener('load',function(){
+		var tm2 = (new Date()).getTime();
+		var gap = tm2-tm;
+		console.log("%cWelcome! "+document.location.hostname+"\n%cHTML LOADING TIME : "+gap+" ms","font-size:20px;color:#abc;","font-size:8px;color:#ccc;");
+	});
+})();
