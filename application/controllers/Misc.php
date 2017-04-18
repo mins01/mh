b92e524772be601a47d27e3ce84c56d8d37a0f02
mins01 <mins01.lycos.co.kr@gmail.com>
2017-04-18 21:15:28 +0900

    웹캐시 처리.

--- application/controllers/Misc.php
+++ application/controllers/Misc.php
@@ -19,8 +19,27 @@ class Misc extends MX_Controller {
 	}
 
 	public function htmlOgp(){
+
 		//file_get_contents("https://www.youtube.com/watch?v=EIGGsZZWzZA")
 		$url = $this->input->get('url');
+
+		//-- 웹 캐시 설정
+		$this->load->library('mheader');
+		$sec = 60*60*24; //하루. 더 길게해도 문제 없다.(파일 수정 기능이 없기 때문에)
+		$etag = date('Hi').ceil(date('s')/$sec).substr(md5($url),0,6);
+
+		//$msgs = array();
+		if( MHeader::etag($etag)){ //etag는 사용하지 말자.
+		//$msgs[] = 'etag 동작';//실제 출력되지 않는다.(304 발생이 되기 때문에)
+			exit('etag 동작');
+		}else if(MHeader::lastModified($sec)){
+		//$msgs[] = 'lastModified 동작'; //실제 출력되지 않는다.(304 발생이 되기 때문에)
+			exit('lastModified 동작');
+		}
+		MHeader::expires($sec);
+
+
+
 		if(!$url){
 			show_error("required url");
 		}
@@ -45,20 +64,7 @@ class Misc extends MX_Controller {
 			// exit;
 		}
 
-		//-- 웹 캐시 설정
-		$this->load->library('mheader');
-		$sec = 60*60*24; //하루. 더 길게해도 문제 없다.(파일 수정 기능이 없기 때문에)
-		$etag = date('Hi').ceil(date('s')/$sec).substr(md5($url),0,6);
 
-		//$msgs = array();
-		if( MHeader::etag($etag)){ //etag는 사용하지 말자.
-		//$msgs[] = 'etag 동작';//실제 출력되지 않는다.(304 발생이 되기 때문에)
-			exit('etag 동작');
-		}else if(MHeader::lastModified($sec)){
-		//$msgs[] = 'lastModified 동작'; //실제 출력되지 않는다.(304 발생이 되기 때문에)
-			exit('lastModified 동작');
-		}
-		MHeader::expires($sec);
 
 		// $this->output->cache($sec/60);
 		$this->load->view('/misc/htmlOgp',array('opgs'=>$opgs,'url'=>$url));
