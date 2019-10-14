fc490ac70d7ecbd2a43b37a3ff008a0c4586536d
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-10-14 18:03:15 +0900

    부트스트랩 3 테마 제외

--- application/modules/mh_admin/views/layout/head.php
+++ application/modules/mh_admin/views/layout/head.php
@@ -15,7 +15,7 @@
 	<!-- 합쳐지고 최소화된 최신 CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 	<!-- 부가적인 테마 -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css"> -->
 	<!-- Jquery : not support < IE9-->
 	<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 	<!-- 합쳐지고 최소화된 최신 자바스크립트 -->
