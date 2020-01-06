856694426024f4838ab1bd73d127de102f38ba68
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-01-06 13:15:05 +0900

    htmlogp 캐시 사용하도록

--- application/controllers/Misc.php
+++ application/controllers/Misc.php
@@ -19,7 +19,6 @@ class Misc extends MX_Controller {
 	}
 
 	public function htmlOgp(){
-
 		//file_get_contents("https://www.youtube.com/watch?v=EIGGsZZWzZA")
 		$url = $this->input->get('url');
 
@@ -39,87 +38,97 @@ class Misc extends MX_Controller {
 		MHeader::expires($sec);
 
 
-
 		if(!$url){
 			show_error("required url");
 		}
-		$this->load->library('Mproxy');
-		$opts = array();
-		$opts[CURLOPT_SSL_VERIFYPEER]=false;
-		$opts[CURLOPT_SSL_VERIFYHOST]=false;
 
-		//($url,$cookieRaw=null,$headers=array(), $opts = array())
-		// $url='//test.com/index.php?123=312';
-		// URL에 생략된 것 처리
-		$t = parse_url($url);
-		// var_dump($t);
-		if(!isset($t['host'][0])){
-			$t['host'] = isset($_SERVER['HTTP_HOST'][0])?$_SERVER['HTTP_HOST']:'';
-		}
-		if(!isset($t['scheme'][0])){
-			if(strpos($url,'//')===0){
-				$t['scheme'] = isset($_SERVER['HTTPS'][0])?'https':'http';
-			}else{
-				$t['scheme'] = 'http';
-			}
-
-		}
-		$url = unparse_url($t);
-		//
-		//
-		// var_dump($t);
-		// var_dump($url);
+		$this->load->module('mh_util/mh_cache');
+		$key = md5($url);
+		// var_dump( $this->mh_cache);
 		// exit;
-		$this->mproxy->conn_timeout = 60;
-		$this->mproxy->exec_timeout = 60;
-		$res = $this->mproxy->get($url,null,array(),$opts);
+		$opgs = $this->mh_cache->get($key);
+		// var_dump($opgs);
+		if(!$opgs){
+			$this->load->library('Mproxy');
+			$opts = array();
+			$opts[CURLOPT_SSL_VERIFYPEER]=false;
+			$opts[CURLOPT_SSL_VERIFYHOST]=false;
 
-		// echo $url;
-		// print_r($res);exit;
-		if($res['errorno']!=0 ){
-			show_error($res['errormsg'],$res['httpcode'],$res['errormsg']);
-		}else if(!isset($res['body'][0]) || stripos('html',$res['body'])!==false){
-			show_error('not html contents');
+			//($url,$cookieRaw=null,$headers=array(), $opts = array())
+			// $url='//test.com/index.php?123=312';
+			// URL에 생략된 것 처리
+			$t = parse_url($url);
+			// var_dump($t);
+			if(!isset($t['host'][0])){
+				$t['host'] = isset($_SERVER['HTTP_HOST'][0])?$_SERVER['HTTP_HOST']:'';
+			}
+			if(!isset($t['scheme'][0])){
+				if(strpos($url,'//')===0){
+					$t['scheme'] = isset($_SERVER['HTTPS'][0])?'https':'http';
+				}else{
+					$t['scheme'] = 'http';
+				}
+
+			}
+			$url = unparse_url($t);
+			//
+			//
+			// var_dump($t);
+			// var_dump($url);
 			// exit;
-		}
-		$content = $res['body'];
-		$charset = 'utf-8';
-		$matches = array();
-		preg_match('/charset=([^\s\n>"\']*)/',$res['header'],$matches);
-		if(isset($matches[1])){
-			$charset = $matches[1];
-		}else{
+			set_time_limit(120);// 동작 타임아웃
+			$this->mproxy->conn_timeout = 60;
+			$this->mproxy->exec_timeout = 60;
+			$res = $this->mproxy->get($url,null,array(),$opts);
+
+			// echo $url;
+			// print_r($res);exit;
+			if($res['errorno']!=0 ){
+				show_error($res['errormsg'],$res['httpcode'],$res['errormsg']);
+			}else if(!isset($res['body'][0]) || stripos('html',$res['body'])!==false){
+				show_error('not html contents');
+				// exit;
+			}
+			$content = $res['body'];
+			$charset = 'utf-8';
 			$matches = array();
-			preg_match('/charset=([^\s\n>]*)/',$content,$matches);
+			preg_match('/charset=([^\s\n>"\']*)/',$res['header'],$matches);
 			if(isset($matches[1])){
 				$charset = $matches[1];
+			}else{
+				$matches = array();
+				preg_match('/charset=([^\s\n>]*)/',$content,$matches);
+				if(isset($matches[1])){
+					$charset = $matches[1];
+				}
 			}
-		}
-		$charset = str_replace(array("'",'"','>'),'',$charset);
-		$charset = strtolower($charset);
-		// echo $charset;
-		$this->load->library('mh_util');
-
-		if($charset == 'ks_c_5601-1987'){
-			$charset = 'uhc';
-		}
-		if($charset != 'utf-8'){
+			$charset = str_replace(array("'",'"','>'),'',$charset);
+			$charset = strtolower($charset);
 			// echo $charset;
-			$content = @iconv($charset,'UTF-8//IGNORE',$content);
-			// echo $content;
+			$this->load->library('mh_util');
+
+			if($charset == 'ks_c_5601-1987'){
+				$charset = 'uhc';
+			}
+			if($charset != 'utf-8'){
+				// echo $charset;
+				$content = @iconv($charset,'UTF-8//IGNORE',$content);
+				// echo $content;
+			}
+			// echo $content ;
+			$opgs = $this->mh_util->parseOgp($content,$url);
+			// var_dump($opgs);
+			if(empty($opgs)){
+				$opgs['title']='not opg meta!';
+				// show_error("not opg meta");
+				// exit;
+			}
+
+			// $this->output->cache($sec/60);
+			$this->mh_cache->save($key,$opgs);
+		}else{
+
 		}
-		// echo $content ;
-		$opgs = $this->mh_util->parseOgp($content,$url);
-		// var_dump($opgs);
-		if(empty($opgs)){
-			$opgs['title']='not opg meta!';
-			// show_error("not opg meta");
-			// exit;
-		}
-
-
-
-		// $this->output->cache($sec/60);
 		$this->load->view('/misc/htmlOgp',array('opgs'=>$opgs,'url'=>$url));
 	}
 
