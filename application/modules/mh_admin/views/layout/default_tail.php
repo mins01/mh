7a87f1858839a0366fdf85d4d881e8fdf7232b4b
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-02-28 13:12:34 +0900

    관리자용 기본 레이아웃

---
+++ application/modules/mh_admin/views/layout/default_tail.php
@@ -0,0 +1,13 @@
+<? if(!$hide): ?>
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+	<footer>
+		<div class="container-fluid" style="background-color:#eef;">
+			<div class="text-right text-small"> <small>[{elapsed_time} sec]/[{memory_usage} Byte]</small></div>
+		</div>
+	</footer>
+<?=$tail_contents?>
+
+<? endif; ?>
+</body>
+</html>
