96e69d62603a311a8be48382725520a72ffc0dff
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-04-06 14:10:19 +0900

    관리자용 index 추가.

---
+++ application/modules/mh_admin/views/page/admin_index.php
@@ -0,0 +1,8 @@
+<?
+//$bm_row,$b_rows,$b_n_rows
+//$start_num,$count
+// $json_url = dirname($conf['base_url']).'/'.$conf['menu']['mn_arg2'];
+?>
+<h4>메뉴를 선택해주세요.</h4>
+<div style="min-height:300px;">
+</div>
