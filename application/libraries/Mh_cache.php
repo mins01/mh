9db3999e05152e73e286dd64982f6cde03b6d06c
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-04-16 11:19:29 +0900

    mh_cache->use_log_header 로그 해더 를 기본으로는 출력하지 않음 해더가 너무 많으면 해더 사이즈가 너무 커져서 이슈가 됨

--- application/libraries/Mh_cache.php
+++ application/libraries/Mh_cache.php
@@ -8,6 +8,7 @@ class Mh_cache {
 	private $act_count = 0;
   private $CI = 0;
   public $use_cache = false; //사용유무
+  public $use_log_header = false; //해더로 로그 출력
 	public function __construct()
 	{
     $this->CI =& get_instance();
@@ -15,14 +16,19 @@ class Mh_cache {
     $this->use_cache = USE_MH_CACHE;
 		//print_r($this->CI->cache);
 	}
+  public function header($msg){
+    if($this->use_log_header){
+      @header($msg);
+    }
+  }
 	public function get($key){
 		if(!$this->use_cache){return null;}
 		$key = preg_replace('/[^\w\s\.]/','_',$key);
 		$r = $this->CI->cache->get($key);
 		if($r!==false){
-			header("X-Cache-{$this->act_count}: [Cached] {$key}");
+			$this->header("X-Cache-{$this->act_count}: [Cached] {$key}");
 		}else{
-			header("X-Cache-{$this->act_count}: [No-Cached] {$key}");
+			$this->header("X-Cache-{$this->act_count}: [No-Cached] {$key}");
 		}
 		$this->act_count++;
 		return $r;
@@ -32,9 +38,9 @@ class Mh_cache {
 		$key = preg_replace('/[^\w\s\.]/','_',$key);
 		$r = $this->CI->cache->save($key,$val,$ttl);
 		if(!$r){
-			header("X-Cache-{$this->act_count}: [No-Saved] {$key} ({$ttl})");
+			$this->header("X-Cache-{$this->act_count}: [No-Saved] {$key} ({$ttl})");
 		}else{
-			header("X-Cache-{$this->act_count}: [Saved] {$key} ({$ttl})");
+			$this->header("X-Cache-{$this->act_count}: [Saved] {$key} ({$ttl})");
 		}
 		$this->act_count++;
 		return $r;
@@ -44,9 +50,9 @@ class Mh_cache {
 		$key = preg_replace('/[^\w\s\.]/','_',$key);
 		$r = $this->CI->cache->delete($key);
 		if(!$r){
-			header("X-Cache-{$this->act_count}: [No-Deleted] {$key}");
+			$this->header("X-Cache-{$this->act_count}: [No-Deleted] {$key}");
 		}else{
-			header("X-Cache-{$this->act_count}: [Deleted] {$key}");
+			$this->header("X-Cache-{$this->act_count}: [Deleted] {$key}");
 		}
 		$this->act_count++;
 		return $r;
