ca6aef50d783f9c684813e0b13256bb97f018c49
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-09-28 13:36:23 +0900

    ga_dashboard 에 V4 적용

--- application/modules/mh_admin/controllers/Ga_dashboard.php
+++ application/modules/mh_admin/controllers/Ga_dashboard.php
@@ -19,8 +19,12 @@ class Ga_dashboard extends MX_Controller {
 		$this->googleoauth2->set_access_token($conf_google_oauth2['access_tokens']['analytics.readonly']);
 		$this->cache_key = 'analytics.readonly';
 
-		$this->load->library('GoogleAnalyticsApi');
-		$this->googleanalyticsapi->set_mproxy($this->mproxy);
+		// $this->load->library('GoogleAnalyticsApi');
+		// $this->googleanalyticsapi->set_mproxy($this->mproxy);
+
+		$this->load->library('GoogleAnalyticsReportingApiV4');
+		$this->googleanalyticsreportingapiv4->set_mproxy($this->mproxy);
+		// $this->googleanalyticsreportingapiv4->set_access_token($access_token['access_token']);
 
 		$this->profileId = $conf['menu']['mn_arg1'];
 		$this->daysAgo = isset($conf['menu']['mn_arg2'][0])?$conf['menu']['mn_arg2']:14;
@@ -84,60 +88,149 @@ class Ga_dashboard extends MX_Controller {
 			)
 		);
 	}
-
 	private function get_ga_rowss($profileId,$daysAgo){
 		$rowss = array();
 		$rowss['createdAt'] = date('Y-m-h H:i:s');
 		$access_token = $this->get_access_token(); //캐싱 해야함!! 꼭 60분 캐싱하자
-		$this->googleanalyticsapi->set_access_token($access_token);
-
-
-		$res = array('rows'=>array());
-		//--- 검색어 7일간 TOP10
-		$gets = array(
-			'ids'=> 'ga:'.$profileId,
-			'start-date'=> $daysAgo.'daysAgo',
-			'end-date'=> 'today',
-			// 검색어용
-			'dimensions'=> 'ga:searchKeyword,ga:pagePath',
-			'metrics'=> 'ga:searchResultViews,ga:searchUniques',  //PV,UPV
-			'sort'=> '-ga:searchUniques',
-			'max-results'=> 10
+		$this->googleanalyticsreportingapiv4->set_access_token($access_token);
+		$posts = array(
+			'reportRequests'=>array(
+				// USER,NEW USER,PV,UPV per date
+				array(
+					'viewId'=>$profileId,
+					'dateRanges'=>array(
+						"startDate"=>$daysAgo.'daysAgo',
+						"endDate"=>"today"
+					),
+					'dimensions'=>array(
+						array("name"=>"ga:date")
+					),
+					'metrics'=>array(
+						array("expression"=>"ga:users"),
+						array("expression"=>"ga:newUsers"),
+						array("expression"=>"ga:pageviews"),
+						array("expression"=>"ga:uniquePageviews"),
+					),
+					// 'orderBys'=>array(
+					// 	array(
+					// 		"fieldName"=> "ga:pageviews",
+					// 		"sortOrder"=> "DESCENDING"
+					// 	)
+					// ),
+					'samplingLevel'=>'SMALL', //SMALL , LARGE, DEFAULT
+					// 'segments'=>array(
+					// 	array(
+					// 		"segmentId"=> "gaid::-11"
+					// 	)
+					// ),
+					// 'dimensionFilterClauses'=>array(
+					// 	'filters'=>array(
+					// 		"dimension_name"=>"ga:browser",
+					// 		"operator"=>"EXACT",
+					// 		"expressions"=>array("Firefox"),
+					// 	)
+					// ),
+					// "filtersExpression"=> "ga:browser==Firefox",
+					// "includeEmptyRows"=> "true",
+					// "pageToken"=>"10000",
+					"pageSize"=> 365*2,
+				),
+				//-- PAGE PV
+				array(
+					'viewId'=>$profileId,
+					'dateRanges'=>array(
+						"startDate"=>$daysAgo.'daysAgo',
+						"endDate"=>"today"
+					),
+					'dimensions'=>array(
+						array("name"=>"ga:pagePath"),
+						array("name"=>"ga:pageTitle")
+					),
+					'metrics'=>array(
+						array("expression"=>"ga:pageviews"),
+						array("expression"=>"ga:uniquePageviews"),
+					),
+					'orderBys'=>array(
+						array(
+							"fieldName"=> "ga:pageviews",
+							"sortOrder"=> "DESCENDING"
+						)
+					),
+					'samplingLevel'=>'SMALL', //SMALL , LARGE, DEFAULT
+					// 'segments'=>array(
+					// 	array(
+					// 		"segmentId"=> "gaid::-11"
+					// 	)
+					// ),
+					// 'dimensionFilterClauses'=>array(
+					// 	'filters'=>array(
+					// 		"dimension_name"=>"ga:browser",
+					// 		"operator"=>"EXACT",
+					// 		"expressions"=>array("Firefox"),
+					// 	)
+					// ),
+					// "filtersExpression"=> "ga:browser==Firefox",
+					// "includeEmptyRows"=> "true",
+					// "pageToken"=>"10000",
+					"pageSize"=>"10",
+				),
+				//-- SEARCH PV
+				array(
+					'viewId'=>$profileId,
+					'dateRanges'=>array(
+						"startDate"=>$daysAgo.'daysAgo',
+						"endDate"=>"today"
+					),
+					'dimensions'=>array(
+						array("name"=>"ga:searchKeyword"),
+						array("name"=>"ga:pagePath")
+					),
+					'metrics'=>array(
+						array("expression"=>"ga:searchResultViews"),
+						array("expression"=>"ga:searchUniques"),
+					),
+					'orderBys'=>array(
+						array(
+							"fieldName"=> "ga:searchResultViews",
+							"sortOrder"=> "DESCENDING"
+						)
+					),
+					'samplingLevel'=>'SMALL', //SMALL , LARGE, DEFAULT
+					// 'segments'=>array(
+					// 	array(
+					// 		"segmentId"=> "gaid::-11"
+					// 	)
+					// ),
+					// 'dimensionFilterClauses'=>array(
+					// 	'filters'=>array(
+					// 		"dimension_name"=>"ga:browser",
+					// 		"operator"=>"EXACT",
+					// 		"expressions"=>array("Firefox"),
+					// 	)
+					// ),
+					// "filtersExpression"=> "ga:pagePath=~.*/list?.*", //목록만 검색
+					// "includeEmptyRows"=> "true",
+					// "pageToken"=>"10000",
+					"pageSize"=>"10",
+				),
+			)
 		);
-		$res = $this->googleanalyticsapi->data_ga($gets);
-		$rowss['searchs'] = $res['rows'];
-		$rowss['total_search'] = $res['totalsForAllResults'];
-
-		//--- Page 7일간 TOP10
-		$gets = array(
-			'ids'=> 'ga:'.$profileId,
-			'start-date'=> $daysAgo.'daysAgo',
-			'end-date'=> 'today',
-			// 검색어용
-			'dimensions'=> 'ga:pagePath,ga:pageTitle',
-			'metrics'=> 'ga:pageviews,ga:uniquePageviews',  //PV,UPV
-			'sort'=> '-ga:uniquePageviews',
-			'max-results'=> 10
-		);
-		$res = $this->googleanalyticsapi->data_ga($gets);
-		$rowss['pages'] = $res['rows'];
+		$res = $this->googleanalyticsreportingapiv4->data_ga($posts);
 		// print_r($res);exit;
-		$rowss['total_page'] = $res['totalsForAllResults'];
+		$res_rowss = $this->googleanalyticsreportingapiv4->extract_rowss($res);
+		// print_r($rowss);exit;
 
-		//--- 방문자 7일간 TOP10
-		$gets = array(
-			'ids'=> 'ga:'.$profileId,
-			'start-date'=> $daysAgo.'daysAgo',
-			'end-date'=> 'today',
-			'dimensions'=> 'ga:date',
-			'metrics'=> 'ga:users,ga:newUsers',  //방문자,신규방문자
-			'max-results'=> 365*2
+		$rowss = array(
+			'per_date' => $res_rowss[0],
+			'pages' => $res_rowss[1],
+			'searchs' => $res_rowss[2],
+			'total_per_date' => $res['reports'][0]['data']['totals'][0]['values'],
+			'total_pages' => $res['reports'][1]['data']['totals'][0]['values'],
+			'total_searchs' => $res['reports'][2]['data']['totals'][0]['values'],
+			'createdAt' => date('Y-m-h H:i:s'),
 		);
-		$res = $this->googleanalyticsapi->data_ga($gets);
-		// print_r($res);
+		// print_r($rowss);
 		// exit;
-		$rowss['users'] = $res['rows'];
-		$rowss['total_user'] = $res['totalsForAllResults'];
 		return $rowss;
 	}
 
@@ -186,7 +279,116 @@ class Ga_dashboard extends MX_Controller {
 	}
 
 
+	public function analytics_test_v4($conf,$param){
+		$access_token = $this->googleoauth2->refreshed_access_token(); //캐싱 해야함!! 꼭 60분 캐싱하자
+		header('Content-Type: application/json');
 
+		$this->load->library('GoogleAnalyticsReportingApiV4');
+		$this->googleanalyticsreportingapiv4->set_mproxy($this->mproxy);
+		$this->googleanalyticsreportingapiv4->set_access_token($access_token['access_token']);
+		// $res = $this->googleanalyticsapi->accountSummaries();
+		//-- ex
+		$profileId = '54658549'; //Lee Minsu/mins01.com/homepage
+
+
+		$posts = array(
+			'reportRequests'=>array(
+					array(
+						'viewId'=>$profileId,
+						'dateRanges'=>array(
+							"startDate"=>"7daysAgo",
+							"endDate"=>"yesterday"
+						),
+						'dimensions'=>array(
+							array("name"=>"ga:searchKeyword"),
+							array("name"=>"ga:date")
+						),
+						'metrics'=>array(
+							array("expression"=>"ga:searchResultViews"),
+							array("expression"=>"ga:searchUniques"),
+							array("expression"=>"ga:searchExitRate"),
+							array("expression"=>"ga:percentSearchRefinements"),
+							array("expression"=>"ga:avgSearchDuration"),
+							array("expression"=>"ga:avgSearchDepth"),
+						),
+						'orderBys'=>array(
+							array(
+								"fieldName"=> "ga:searchUniques",
+								"sortOrder"=> "DESCENDING"
+							)
+						),
+						'samplingLevel'=>'LARGE', //SMALL , LARGE, DEFAULT
+						// 'segments'=>array(
+						// 	array(
+						// 		"segmentId"=> "gaid::-11"
+						// 	)
+						// ),
+						// 'dimensionFilterClauses'=>array(
+						// 	'filters'=>array(
+						// 		"dimension_name"=>"ga:browser",
+						// 		"operator"=>"EXACT",
+						// 		"expressions"=>array("Firefox"),
+						// 	)
+						// ),
+						// "filtersExpression"=> "ga:browser==Firefox",
+						// "includeEmptyRows"=> "true",
+						// "pageToken"=>"10000",
+						"pageSize"=>"10",
+					),
+					array(
+						'viewId'=>$profileId,
+						'dateRanges'=>array(
+							"startDate"=>"7daysAgo",
+							"endDate"=>"yesterday"
+						),
+						// 	// 'dimensions'=> 'ga:pagePath,ga:date',
+						// 	// 'metrics'=> 'ga:pageviews,ga:uniquePageviews,ga:avgTimeOnPage,ga:entrances,ga:bounceRate,ga:exitRate',
+						// 	// 'sort'=> '-ga:pageviews',
+						'dimensions'=>array(
+							array("name"=>"ga:pagePath"),
+							array("name"=>"ga:date")
+						),
+						'metrics'=>array(
+							array("expression"=>"ga:pageviews"),
+							array("expression"=>"ga:uniquePageviews"),
+							array("expression"=>"ga:avgTimeOnPage"),
+							array("expression"=>"ga:entrances"),
+							array("expression"=>"ga:bounceRate"),
+							array("expression"=>"ga:exitRate"),
+						),
+						'orderBys'=>array(
+							array(
+								"fieldName"=> "ga:pageviews",
+								"sortOrder"=> "DESCENDING"
+							)
+						),
+						'samplingLevel'=>'LARGE', //SMALL , LARGE, DEFAULT
+						// 'segments'=>array(
+						// 	array(
+						// 		"segmentId"=> "gaid::-11"
+						// 	)
+						// ),
+						// 'dimensionFilterClauses'=>array(
+						// 	'filters'=>array(
+						// 		"dimension_name"=>"ga:browser",
+						// 		"operator"=>"EXACT",
+						// 		"expressions"=>array("Firefox"),
+						// 	)
+						// ),
+						// "filtersExpression"=> "ga:browser==Firefox",
+						// "includeEmptyRows"=> "true",
+						// "pageToken"=>"10000",
+						"pageSize"=>"10",
+					),
+			)
+		);
+		$res = $this->googleanalyticsreportingapiv4->data_ga($posts);
+		$rowss = $this->googleanalyticsreportingapiv4->extract_rowss($res);
+
+		print_r($rowss);
+		print_r($res);
+		exit;
+	}
 
 
 
--- application/modules/mh_admin/views/ga_dashboard/dashboard.php
+++ application/modules/mh_admin/views/ga_dashboard/dashboard.php
@@ -1,4 +1,13 @@
 <?
+// $rowss = array(
+// 	'per_date' => $res_rowss[0],
+// 	'pages' => $res_rowss[1],
+// 	'searchs' => $res_rowss[2],
+// 	'total_per_date' => $res[0]['data']['totals'],
+// 	'total_pages' => $res[1]['data']['totals'],
+// 	'total_searchs' => $res[2]['data']['totals'],
+// 	'createdAt' => date('Y-m-h H:i:s'),
+// );
 // $rowss['searchs']
 // $rowss['total_search']
 // $rowss['pages']
@@ -9,62 +18,39 @@
 //
 // print_r($rowss['total_page']);
 ?>
-<h2 style="margin-bottom:2em" class="text-center">Google analytics 대쉬보드 ( <?=$daysAgo?>일간 )</h2>
-<div class="text-center" style="font-size:20px;">
-방문자 : <?=number_format($rowss['total_user']['ga:users'])?> /
-신규방문자 : <?=number_format($rowss['total_user']['ga:newUsers'])?> (<?=round($rowss['total_user']['ga:newUsers']/$rowss['total_user']['ga:users']*100)?>%)
+<h2 style="margin-bottom:1em" class="text-center">Google analytics 대쉬보드 ( <?=$daysAgo?>일간 )</h2>
+<div class="text-center">
+	<table class="table table-striped table-bordered table-condensed" style="width:auto; margin:0 auto;">
+		<thead>
+			<tr>
+				<th class="text-center">방문자</th>
+				<th class="text-center">신규방문자</th>
+				<th class="text-center">PV</th>
+				<th class="text-center">UPV</th>
+			</tr>
+		</thead>
+
+		<tr>
+			<td><?=number_format($rowss['total_per_date'][0])?></td>
+			<td><?=number_format($rowss['total_per_date'][1])?> (<?=round($rowss['total_per_date'][1]/$rowss['total_per_date'][0]*100)?>%)</td>
+			<td><?=number_format($rowss['total_per_date'][2])?></td>
+			<td><?=number_format($rowss['total_per_date'][3])?> (<?=round($rowss['total_per_date'][3]/$rowss['total_per_date'][2]*100)?>%)</td>
+		</tr>
+	</table>
 </div>
 <div id="curve_chart" style="width: 100%; max-width: 1000px;height: 450px;margin:10px auto; overflow-x:auto; overflow-y:hidden"></div>
 <div class="row">
 	<div class="col-lg-6">
 		<table class="table" style="table-layout:fixed">
 			<colgroup>
-				<col width="80">
-				<col>
-				<col width="80">
-				<col width="80">
-			</colgroup>
-			<thead>
-				<tr>
-					<th class="text-center">번호</th>
-					<th class="text-center">검색어</th>
-					<th class="text-center">PV</th>
-					<th class="text-center">UPV</th>
-				</tr>
-			</thead>
-			<tfoot>
-				<tr>
-					<th class="text-center">총</th>
-					<th class="text-center"></th>
-					<th class="text-center"><?=html_escape(number_format($rowss['total_search']['ga:searchResultViews']))?></th>
-					<th class="text-center"><?=html_escape(number_format($rowss['total_search']['ga:searchUniques']))?></th>
-				</tr>
-			</tfoot>
-			<?
-			foreach ($rowss['searchs'] as $k => $row):
-				?>
-				<tr>
-					<td class="text-center"><?=$k+1?></td>
-					<td class="text-left text-overflow-ellipsis"><a href="<?=html_escape($row[1])?>" target="_blank"><?=html_escape($row[0])?></a></td>
-					<td class="text-center"><?=html_escape(number_format($row[2]))?></td>
-					<td class="text-center"><?=html_escape(number_format($row[3]))?></td>
-				</tr>
-				<?
-			endforeach;
-			?>
-		</table>
-	</div>
-	<div class="col-lg-6">
-		<table class="table" style="table-layout:fixed">
-			<colgroup>
-				<col width="80">
+				<col width="30">
 				<col>
 				<col width="80">
 				<col width="80">
 			</colgroup>
 			<thead >
 				<tr >
-					<th class="text-center">번호</th>
+					<th class="text-center">no</th>
 					<th class="text-center">페이지</th>
 					<th class="text-center">PV</th>
 					<th class="text-center">UPV</th>
@@ -74,8 +60,8 @@
 				<tr>
 					<th class="text-center">총</th>
 					<th class="text-center"></th>
-					<th class="text-center"><?=html_escape(number_format($rowss['total_page']['ga:pageviews']))?></th>
-					<th class="text-center"><?=html_escape(number_format($rowss['total_page']['ga:uniquePageviews']))?></th>
+					<th class="text-center"><?=html_escape(number_format($rowss['total_pages'][0]))?></th>
+					<th class="text-center"><?=html_escape(number_format($rowss['total_pages'][1]))?></th>
 				</tr>
 			</tfoot>
 			<?
@@ -93,15 +79,54 @@
 		</table>
 
 	</div>
+	<div class="col-lg-6">
+		<table class="table" style="table-layout:fixed">
+			<colgroup>
+				<col width="30">
+				<col>
+				<col width="80">
+				<col width="80">
+			</colgroup>
+			<thead>
+				<tr>
+					<th class="text-center">no</th>
+					<th class="text-center">검색어</th>
+					<th class="text-center">PV</th>
+					<th class="text-center">UPV</th>
+				</tr>
+			</thead>
+			<tfoot>
+				<tr>
+					<th class="text-center">총</th>
+					<th class="text-center"></th>
+					<th class="text-center"><?=html_escape(number_format($rowss['total_searchs'][0]))?></th>
+					<th class="text-center"><?=html_escape(number_format($rowss['total_searchs'][1]))?></th>
+				</tr>
+			</tfoot>
+			<?
+			foreach ($rowss['searchs'] as $k => $row):
+				?>
+				<tr>
+					<td class="text-center"><?=$k+1?></td>
+					<td class="text-left text-overflow-ellipsis"><a href="<?=html_escape($row[1])?>" target="_blank"><?=html_escape($row[0])?></a></td>
+					<td class="text-center"><?=html_escape(number_format($row[2]))?></td>
+					<td class="text-center"><?=html_escape(number_format($row[3]))?></td>
+				</tr>
+				<?
+			endforeach;
+			?>
+		</table>
+	</div>
+
 </div>
 <div class="text-right">data date : <?=html_escape($rowss['createdAt'])?></div>
 
 <?
 // print_r($rowss['users']);
-$chart_title = array('날짜','방문자','신규방문자');
+$chart_title = array('날짜','방문자','신규방문자','PV','UPV');
 $chart_value = array();
-foreach ($rowss['users'] as $k => $r) {
-	$chart_value[] = array(date('m-d',strtotime($r[0])),(int)$r[1],(int)$r[2]);
+foreach ($rowss['per_date'] as $k => $r) {
+	$chart_value[] = array(date('m-d',strtotime($r[0])),(int)$r[1],(int)$r[2],(int)$r[3],(int)$r[4]);
 }
 $chart_data = array_merge(array($chart_title),$chart_value);
 // print_r($chart_data);
